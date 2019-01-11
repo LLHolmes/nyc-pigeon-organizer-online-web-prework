60dced62d2322b50279bcf040bf393ce3c730619
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-11 21:17:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |key, options|
+    options.each do |type, pigeon|
+      pigeon.each do |name|
+        if organized.keys.include?(name)
+          if organized[name].keys.include?(key)
+            
+          else
+        else
+          organized[name] = { key => type }
 end
